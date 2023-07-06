397561cab160cf34694bfa64bad116d97b6c0735
Сергей Коноплёв <konoplyow.sergei@yandex.ru>
2023-07-06 12:14:19 +0400

    Добавил epicID в вывод подзадачи

--- src/Tasks/SubTask.java
+++ src/Tasks/SubTask.java
@@ -4,6 +4,17 @@ public class SubTask extends Task {
 
     private int epicId;
 
+    @Override
+    public String toString() {
+        return "SubTask{" +
+                "epicId=" + epicId +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+
     public SubTask(String name, String description, int epicId) {
         super(name, description);
         this.epicId = epicId;
