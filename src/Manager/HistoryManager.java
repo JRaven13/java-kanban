2d006830bbb2ff213f77c40c7fe4da99de49dac3
Сергей Коноплёв <konoplyow.sergei@yandex.ru>
2023-08-12 15:40:59 +0400

    Узнать что я сделал не так

--- src/Manager/HistoryManager.java
+++ src/Manager/HistoryManager.java
@@ -5,10 +5,10 @@ import Tasks.Task;
 import java.util.List;
 
 public interface HistoryManager {
-
     List<Task> getHistory();
 
     void addTask(Task task);
 
+    void remove(int id);
 
 }
--- src/Manager/InMemoryHistoryManager.java
+++ src/Manager/InMemoryHistoryManager.java
@@ -2,19 +2,29 @@ package Manager;
 
 import Tasks.Task;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private static final int HISTORY_SIZE = 10;
+    Node first;
+    Node last;
 
-    private final LinkedList<Task> history = new LinkedList<>();
+    Map<Integer, Node> map = new HashMap<>();
 
     @Override
     public List<Task> getHistory() {
-        List<Task> copyHistory = history;
-        return copyHistory;
+        List<Task> result = new ArrayList<>();
+        if (first == null) {
+            System.out.println("Список пуст!");
+            return result;
+        }
+        Node current = first;
+
+        while (current != null) {
+            result.add(current.getValue());
+            current = current.next;
+        }
+        return result;
     }
 
     @Override
@@ -22,9 +32,73 @@ public class InMemoryHistoryManager implements HistoryManager {
         if (task == null) {
             return;
         }
-        history.add(task);
-        if (history.size() > HISTORY_SIZE) {
-            history.removeFirst();
+        Node node = new Node(task);
+
+        if (last != null) {
+            last.next = node;
+            node.prev = last;
+            last = node;
+            map.put(map.size()+1, node);
+        } else {
+            first = node;
+            last = node;
+            map.put(1, node);
+        }
+    }
+
+    @Override
+    public void remove(int id) {
+        Node current = map.get(id);
+        map.remove(id);
+
+        if (current == last && current == first) {
+            last = null;
+            first = null;
+        } else if (current == last) {
+            current.prev.next = null;
+            last = current.prev;
+            current.next = null;
+            current.prev = null;
+        } else if (current == first) {
+            current.next.prev = null;
+            first = current.next;
+            current.next = null;
+            current.prev = null;
+        } else {
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+        }
+    }
+
+
+    public static class Node {
+
+        Task value;
+        Node next;
+        Node prev;
+
+        public Node(Task value) {
+            this.value = value;
+        }
+
+        public Task getValue() {
+            return value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
         }
     }
 }
